a451a52705502563865fb4a2413d573bf095b05b
Ukpono Obott <37601569+ukponoobott@users.noreply.github.com>
2023-03-10 16:43:31 -0500

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -1,3 +1,3 @@
-from pypaystack.transactions import Transaction
+# from pypaystack.transactions import Transaction
 
-tranaction = Transaction(authorization_key="sk_test_8e81c920217c39de48c778ca688c97f23035f86a")
+# tranaction = Transaction(authorization_key="sk_test_8e81c920217c39de48c778ca688c97f23035f86a")
