8dc4e5d3b61b7220ef2455a98dc3147ef36042f1
Ukpono Obott <legendsergio@gmail.com>
2023-03-11 06:38:27 -0500

    Local changes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import requests
 from flask import Flask, redirect, render_template, request
-from transaction import tranaction
 
 app = Flask(__name__)
 
@@ -10,9 +9,9 @@ def index():
 
 
 items = [
-    {"name": "shoe", "price": 100},
-    {"name": "earphone", "price": 40.00},
-    {"name": "watch", "price": 500}
+    {"name": "shoe", "price": 100000},
+    {"name": "earphone", "price": 40000},
+    {"name": "watch", "price": 500000}
 ]
 
 ref = ""
@@ -24,12 +23,18 @@ access_token = "sk_test_8e81c920217c39de48c778ca688c97f23035f86a"
 @app.route("/payment", methods=["GET", 'POST'])
 def pay():
     if request.method == "POST":
-        price = float(request.form["price"])
-        params = {"amount": amount, "email": "legendsergio@gmail.com"}
+        price = int(request.form["price"])
+        print(price)
+        params = {}
+        print(params)
         try:
-            res = requests.post(url, headers={'Content-Type':'application/json', 'Authorization': 'Bearer {}'.format(access_token)})
-            ref = res[3]["reference"]
-            payment_url = init[3]['authorization_url']
+            res = requests.post(url, data={"amount": price, "email": "legendsergio@gmail.com"}, headers={'Authorization': 'Bearer {}'.format(access_token)})
+            print(res.json())
+            # ref = res[3]["reference"]
+            res_data = res.json()
+            payment_url = res_data['data']['authorization_url']
+            print(payment_url)
+            # payment_url = res[3]['authorization_url']
             return redirect(payment_url)
         except Exception as err:
             print(err)
